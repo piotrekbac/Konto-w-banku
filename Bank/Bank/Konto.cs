9448fdc368d369db374ce49f4283da55e3acb198
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-03-22 22:24:04 +0100

    commit 1

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,23 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string klient;              //nazwa klienta
+        private decimal bilans;             //aktualny stan środków na koncie 
+        private bool zablokowane = false;           //stan konta 
+
+        //private void UtworzKonto() { }    //metoda tworząca konto w podkroku 3 kroku 1 z treści - niepotrzebna
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+            
+        public string Nazwa => klient;
+        public decimal Bilans => bilans;
+        public bool Zablokowane => zablokowane;
+
+
+    }
+
+}
